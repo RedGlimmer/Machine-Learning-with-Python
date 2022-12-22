69e4082b206e6b5316d5b90d5ffb68ef4e44b84c
RedGlimmer <107247457+RedGlimmer@users.noreply.github.com>
2022-12-22 20:42:23 +0100

    Rock paper scissors done

--- Rock paper scissors/RPS.py
+++ Rock paper scissors/RPS.py
@@ -6,17 +6,18 @@ def player(prev_play, opponent_history=[], my_history=['']):
   opponent_history.append(prev_play)
 
   # level of insight
-  level = 5
+  level = 3
   # Predict opponent's next move
   for i in range(len(opponent_history)):
     match = 0
     for j in range(level):
       if i - j > 0 and opponent_history[-1] == opponent_history[i]:
-        if opponent_history[-j-1] == opponent_history[i-j]:
-          if my_history[-j-1] == my_history[i-j]:
-            match += level
+        if opponent_history[-1 - j] == opponent_history[i - j]:
+          if my_history[-1 - j] == my_history[i - j]:
+            match += 1
     if match >= level and i + 2 <= len(opponent_history):
       predict = opponent_history[i+1]
+      break
 
   # Act according to prediction
   if predict == "R":
