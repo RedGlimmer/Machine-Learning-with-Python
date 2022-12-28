c630350410f0b8e08f46b0f2566729b373f11988
RedGlimmer <107247457+RedGlimmer@users.noreply.github.com>
2022-12-28 16:19:23 +0100

    Rock paper scissors done

--- Rock paper scissors/RPS.py
+++ Rock paper scissors/RPS.py
@@ -1,43 +1,6 @@
 def player(prev_play, opponent_history=[], my_history=['']):
   guess = "S"
   predict = None
-
-  if len(opponent_history) > 500:
-    del opponent_history[0]
-    del my_history[0]
-
-  # Opponent moves history
-  opponent_history.append(prev_play)
-
-  # Predict opponent's next move
-  for i in range(len(opponent_history)):
-    match = 0
-    for j in range(3):
-      if i - j > 0 and opponent_history[-1] == opponent_history[i]:
-        if opponent_history[-1 - j] == opponent_history[i - j]:
-          if my_history[-1 - j] == my_history[i - j]:
-            match += 1
-    if match >= 3 and i + 2 <= len(opponent_history):
-      predict = opponent_history[i+1]
-      break
-
-  # Act according to prediction
-  if predict == "R":
-    guess = "P"
-  elif predict == "P":
-    guess = "S"
-  elif predict == "S":
-    guess = "R"
-
-  # My moves history
-  my_history.append(guess)
-  
-  return guess
-
-
-def player_v2(prev_play, opponent_history=[], my_history=['']):
-  guess = "S"
-  predict = None
   
   if len(opponent_history) > 500:
     del opponent_history[0]
--- Rock paper scissors/main.py
+++ Rock paper scissors/main.py
@@ -1,5 +1,5 @@
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
-from RPS import player, player_v2, player_rps
+from RPS import player, player_rps
 
 print("Player:")
 play(player, quincy, 1000)
@@ -7,17 +7,8 @@ play(player, abbey, 1000)
 play(player, kris, 1000)
 play(player, mrugesh, 1000)
 
-print("\nPlayer_v2:")
-play(player_v2, quincy, 1000)
-play(player_v2, abbey, 1000)
-play(player_v2, kris, 1000)
-play(player_v2, mrugesh, 1000)
-
-print("\nplayer_v2 vs player:")
-play(player_v2, player, 1000)
-
-print("\nplayer_v2 vs R-P-S pattern player")
-play(player_v2, player_rps, 1000)
+print("\nplayer vs R-P-S pattern player")
+play(player, player_rps, 1000)
 
 # Uncomment line below to play interactively against a bot:
 # play(human, player_v2, 20, verbose=True)
